544c0a823d1a30e3089532c7ed9a0576088b48ed
dawoon <cdw9079@naver.com>
2023-09-21 14:39:16 +0900

    230921 templates

--- merchant_django/carrot_app/urls.py
+++ merchant_django/carrot_app/urls.py
@@ -6,5 +6,14 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path(r"", views.index, name="index"),
+    path(r"", views.main, name="main"),
+    path(r"index", views.index, name="index"),
+    path(r"chat", views.chat, name="chat"),
+    path(r"location", views.location, name="location"),
+    path(r"search", views.search, name="search"),
+    path(r"trade", views.trade, name="trade"),
+    path(r"trade_post", views.trade_post, name="trade_post"),
+    path(r"write", views.write, name="write"),
+    path(r"test", views.test, name="test"),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
