f873c4712e0d424527452d9067fefbbd181ce3bb
park <wjwjsk12345@gmail.com>
2023-10-04 20:21:39 +0900

    openai 수정

--- merchant_django/merchant_django/settings.py
+++ merchant_django/merchant_django/settings.py
@@ -20,7 +20,7 @@ with open("merchant_django/config.json") as f:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-openai.api_key = "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"
+openai.api_key = json_object["OPENAI_KEY"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
